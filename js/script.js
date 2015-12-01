3505ba10d83dbe5fde63a7948dfbffc5104e1e1a
Kevin Filella <kfilella@espol.edu.ec>
2015-11-30 22:59:48 -0500

    usando drag&drop con svg

--- js/script.js
+++ js/script.js
@@ -1,58 +1,66 @@
 $(document).ready(function() {
-	counter = 0;
-  $('.drag').draggable( {
-    containment: 'droppable',
-    helper: 'clone',
-    stop:function(ev, ui) {
-    	var pos=$(ui.helper).offset();
-    	//console.log(ui.helper);
-    	objName = "#clonediv"+counter
-    	console.log(objName);
-    	$(objName).css({"left":pos.left,"top":pos.top});
-    	$(objName).removeClass("drag");
+	objetos = 0;
+
+	$('.objeto').width($('.objeto').parent().width());
+	$('.objeto').height($('.objeto').parent().height());
+	$(".resizable").resizable();
+
+  	$('.drag').draggable( {
+	    containment: 'canvas',
+	    helper: 'clone',
+	    stop:function(ev, ui) {
+	    	var pos=$(ui.helper).offset();
+	    	//console.log(ui.helper);
+	    	nombre = "#clone"+objetos
+	    	console.log(nombre);
+	    	$(nombre).css({"left":pos.left,"top":pos.top});
+	    	$(nombre).removeClass("drag");
 
 
-       	//When an existiung object is dragged
-        $(objName).draggable({
-        	containment: 'parent',
-            stop:function(ev, ui) {
-            	var pos=$(ui.helper).offset();
-            	console.log($(this).attr("id"));
-				console.log(pos.left)
-                console.log(pos.top)
-            }
-        });
-        //objeto desaparece cuando aplasto rueda del mouse
-        $(objName).addClass("hide"+counter);
-    	$(".hide"+counter).mousedown(function(e){
-	       if( e.button == 1 ) { 
-	      		$(this).fadeOut();
-	      		return false; 
-	    	} 
-	    	return true; 
-	 	}); 
-    }
-  });
-	$("#droppable").droppable({
+	       	//cuando objeto se arrastra
+	        $(nombre).draggable({
+	        	containment: 'parent',
+	            stop:function(ev, ui) {
+	            	var pos=$(ui.helper).offset();
+	            	console.log($(this).attr("id"));
+					console.log(pos.left)
+	                console.log(pos.top)
+	            }
+	        });
+	        //objeto desaparece cuando aplasto rueda del mouse
+	        $(nombre).addClass("hide"+objetos);
+	    	$(".hide"+objetos).mousedown(function(e){
+		       if( e.button == 1 ) { 
+		      		$(this).fadeOut();
+		      		return false; 
+		    	} 
+		    	return true; 
+		 	}); 
+	    }
+	});
+
+	$("#canvas").droppable({
 		drop: function(ev, ui) {
 			console.log(ui.helper);
 			if (ui.helper.attr('id').search(/drag[0-9]/) != -1){
-				counter++;
+				objetos++;
 				var element=$(ui.draggable).clone();
 				element.addClass("tempclass");
 				$(this).append(element);
-				$(".tempclass").attr("id","clonediv"+counter);
-				$("#clonediv"+counter).removeClass("tempclass");
+				$(".tempclass").attr("id","clone"+objetos);
+				$("#clone"+objetos).removeClass("tempclass");
 
 				//Get the dynamically item id
 				draggedNumber = ui.helper.attr('id').search(/drag([0-9])/)
 				itemDragged = "dragged" + RegExp.$1
 				console.log(itemDragged)
 
-				$("#clonediv"+counter).addClass(itemDragged);
-				//$("#clonediv"+counter).addClass("hideable"+counter);
+				$("#clone"+objetos).addClass(itemDragged);
+				
+				//$("#clone"+objetos).addClass("hideable"+objetos);
 			}
 		}
 	});
-    
-});
+
+}); 	
+  
